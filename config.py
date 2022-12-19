9255c649f67e54846450ad3180468f0e8a65faf6
irinabajborodova <bajborodovairina@gmail.com>
2022-12-19 23:59:26 +0300

    Refactoring code and delete dvc

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
+# Пути к файлам
 PATH_APPLICATION_TRAIN = './input/application_train.csv'
 PATH_APPLICATION_TEST = './input/application_test.csv'
 PATH_PREVIOUS_APPLICATION = './input/previous_application.csv'
-
-DEVELOP_DATA_TRAIN_SIZE = 200
-DEVELOP_DATA_TEST_SIZE = 100
-
+# Размер обучаюемого датасета
+DEVELOP_DATA_TRAIN_SIZE = 100
+DEVELOP_DATA_TEST_SIZE = 50
+# Технические моменты
 NFOLDS = 3
 SEED = 0
 NROWS = None
